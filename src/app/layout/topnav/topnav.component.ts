830151ddd71e16f843d424ce929e31b67624cf8a
MuruganBDU <muruganvasu94@gmail.com>
2018-04-06 01:17:23 +0530

    topbar hide show

--- src/app/layout/topnav/topnav.component.ts
+++ src/app/layout/topnav/topnav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../service/login.service';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-topnav',
   templateUrl: './topnav.component.html',
@@ -12,14 +12,16 @@ export class TopnavComponent implements OnInit {
 
   isLoggedIn: boolean=false;
 
-  constructor(private authservice: AuthService) { }
+  constructor(private authservice: AuthService,private router:Router) { }
 
   ngOnInit() {
     this.isLoggedIn = this.authservice.getUserLoggedIn();
   }
 
   onLogout() {
-    this.isLoggedIn = this.authservice.removeUserLoggedIn();
+    //this.isLoggedIn = this.authservice.removeUserLoggedIn();
+    this.authservice.removeUserLoggedIn();
+    this.router.navigate(['/login']);
   }
 
 }
